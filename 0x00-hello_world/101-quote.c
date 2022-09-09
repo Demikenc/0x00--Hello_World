9c8d8c39109884a853fa968ae718b785bb001e70
demikenc <demikenc@yahoo.co.uk>
2022-09-09 04:41:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 
-#include <unistd.h>
+#include<unistd.h>
 
 
 
@@ -8,24 +8,13 @@
  *
  *  * main - Entry point
  *
- *   *
+ *   * Return: Always 1 (Success)
  *
- *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
-
+ *    */
 
 int main(void)
 
 {
-
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
