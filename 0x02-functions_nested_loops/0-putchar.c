73d3fd7590f5f4c8b8dc43185a245c6c9ae688eb
demikenc <demikenc@yahoo.co.uk>
2022-09-13 14:12:26 -0700

    nexted loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	int i;
 		char p[] = "Holberton";
-				for (i = 0; i < 9; i++)
-					{
-						_putchar(p[i]);
-					}
-						_putchar('\n');
+			for (i = 0; i < 9; i++)
+				{
+				_putchar(p[i]);
+				}
+			_putchar('\n');
 return (0);
 }
