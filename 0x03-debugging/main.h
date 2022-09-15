e3c0f5019cb0ab4b91492e06c9adcb06f0b594df
demikenc <demikenc@yahoo.co.uk>
2022-09-15 06:46:01 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H                                                                                                                        
 
 #define MAIN_H                                                                                                                        
-
-                                                                                                                                      
-
-                                                                                                                                  
+void positive_or_negative(int i);                                                                                                                     
 
 int _putchar(char c);                                                                                                                 
 
