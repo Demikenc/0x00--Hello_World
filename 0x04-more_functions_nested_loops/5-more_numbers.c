7797bf5f9433bedd4a87f9caf8e55c5386667877
demikenc <demikenc@yahoo.co.uk>
2022-09-15 20:16:51 -0700

    nexted loops functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,13 @@
 
 void more_numbers(void)
 {
-int i;
-char c;
+	int i;
+	char c;
 	for (i = 0; i < 10; i++)
 	{
-	for (c = 0; c <= 14; c++)
+		for (c = 0; c <= 14; c++)
 		{
-		if (c > 9)
+			if (c > 9)
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
 }			_putchar('\n');												}
