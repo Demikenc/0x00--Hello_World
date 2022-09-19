6c158a8b4301b12926ba20e44844731a2ffb94ed
demikenc <demikenc@yahoo.co.uk>
2022-09-19 07:41:24 -0700

    more nexted loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,11 +23,9 @@ int a, b, op;
 	{
 	_putchar(32);
 	_putchar(32);
-	_putchar(op + 48);
-																	}
-																else if (op <= 99)
-																{
-																_putchar(32);
+	_putchar(op + 48);																}
+else if (op <= 99)
+{																_putchar(32);
 _putchar((op / 10) + 48);
 _putchar((op % 10) + 48);
 }
@@ -36,9 +34,9 @@ else
 _putchar(((op / 100) % 10) + 48);
 _putchar(((op / 10) % 10) + 48);
 _putchar((op % 10) + 48);
-														}
+	}
 		_putchar('\n');
-			}		
-		}	
+			}
+		}
 	}
 }
