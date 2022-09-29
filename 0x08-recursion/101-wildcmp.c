dcd604e24fec08629b745e1189c25bd8dfea7d68
demikenc <demikenc@yahoo.co.uk>
2022-09-28 17:00:12 -0700

    0x08-recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ char *test(char *s5, char *s6, char *t5, char *t6)
 if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
 return (t5);
 else if (*s5 != *s6)
-return (wildcard(s5, t6));
+return (wildcmp(s5, t6));
 else
 return (test(s5 + 1, s6 + 1, t5, t6));
 }
@@ -27,14 +27,14 @@ return (test(s5 + 1, s6 + 1, t5, t6));
 * Return: pointer to spot in s3 that matches s4
 */
 
-char *wildcard(char *s3, char *s4)
+char *wildcmp(char *s3, char *s4)
 
 {
 if (*s3 != *s4)
 {
 if (*s3 == '\0')
 return (0);
-return (wildcard(s3 + 1, s4));
+return (wildcmp(s3 + 1, s4));
 }
 return (test(s3 + 1, s4 + 1, s3, s4));
 }
@@ -56,7 +56,7 @@ else if (*s2 == '*' && *(s2 + 1) == '*')
 return (wildcmp(s1, s2 + 1));
 else if (*s2 == '*')
 {
-p = wildcard(s1, s2 + 1);
+p = wildcmp(s1, s2 + 1);
 if (p == 0)
 return (0);
 return (wildcmp(p + 1, s2 + 2));
