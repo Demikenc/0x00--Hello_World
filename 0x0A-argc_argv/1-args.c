57e5347fd4cd9331ed4e7fc3296bea52db8ead8c
demikenc <demikenc@yahoo.co.uk>
2022-09-30 07:18:49 -0700

    argc filesystem

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints all arguments it recieves
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ * Return: always 0
+ */
 
 
 int main(int argc, char *argv[])
@@ -7,6 +12,6 @@ int main(int argc, char *argv[])
 {
 int counter;
 for (counter = 0; counter < argc; counter++)
-printf ("argv[%2d]: %s\n", counter, argv[counter]);
+printf("argv[%2d]:%s\n", counter, argv[counter]);
 return (0);
 }
