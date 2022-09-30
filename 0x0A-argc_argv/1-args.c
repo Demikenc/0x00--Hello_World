f0dda25a04f54a74c0c13cd4c04cf6d50b9c88ad
demikenc <demikenc@yahoo.co.uk>
2022-09-30 07:04:48 -0700

    argc filesystem

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - prints the name number of arguments passed to it
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- * Return: always 0
- */
+
 
 int main(int argc, char *argv[])
 
 {
-/* to suppress unused variable warning */
-
-argc = argv;
-Printf("%d\n", argc - 1);
-return (0);
+int counter;
+for(counter=0; counter<argc; counter++)
+printf("argv[%2d]: %s\n",counter,argv[counter]);
+return 0;
 }
