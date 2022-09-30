081b5c2444d5f3b00554bc613b95a69d0d70b070
demikenc <demikenc@yahoo.co.uk>
2022-09-30 08:35:52 -0700

    argc filesystem

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,6 +12,6 @@ int main(int argc, char *argv[])
 {
 int counter;
 for (counter = 0; counter < argc; counter++)
-printf("argv[%2d]:%s\n", counter, argv[counter]);
+printf("argv[%2d]:%d\n", counter, *argv[counter]);
 return (0);
 }
