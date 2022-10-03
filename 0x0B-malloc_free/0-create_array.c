0fe8d3c677095a46102a4e845ed78a8773936ce8
demikenc <demikenc@yahoo.co.uk>
2022-10-03 11:29:15 -0700

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,21 @@
 
 
 char *create_array(unsigned int size, char c)
+
 {
 	unsigned int i;
 	char *a;
+
 	if (size == 0)
 		return  (NULL);
 	a = malloc(size * sizeof(char));
-	if (a == NULL)
-	{
-		return(NULL);
-	
-	}
+	if  (a == NULL)
+{
+	return(NULL);
+}
 	for (i = 0; i < size; i++)
 		{
 			a[i] = c;
 		}
 	return (a);
 }
-	
