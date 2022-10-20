daef7b8f3e1403ecbe3be8f01bb93eb8bc78b5bf
demikenc <demikenc@yahoo.co.uk>
2022-10-20 06:51:35 -0700

    Simple listing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 
 /**
- *
  *  * print_list - prints list.
  *
  *   * @h: param list
@@ -19,15 +18,12 @@ size_t print_list(const list_t *h)
 
 			nodelen = 0;
 
-				while (h != NULL)
+	while (h != NULL)
 
-						{
-
-									if (h->str == NULL)
-
-													printf("[%d] %s\n", 0, "(nil)");
-
-											else
+	{
+		if (h->str == NULL)
+		printf("[%d] %s\n", 0, "(nil)");
+	else
 
 															printf("[%d] %s\n", h->len, h->str);
 
