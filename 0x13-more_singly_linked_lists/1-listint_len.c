5f8424aad6483ec7922d173bc768307be6354b41
demikenc <demikenc@yahoo.co.uk>
2022-10-25 14:20:29 -0700

    more singly

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,24 +12,18 @@
  *   *            a linked list_t list.
  *
  *    * @h: The linked list_t list.
- *
- *     *
- *
- *      * Return: The number of elements in h.
- *
+ * Return: The number of elements in h.
  */
 
 size_t list_len(const listint_t *h)
 
 {
 	size_t nodenum = 0;
+
 	while (h)
 	{
 	nodenum++;
 	h = h->next;
 		}
-
-
-
 return (nodenum);
 }
