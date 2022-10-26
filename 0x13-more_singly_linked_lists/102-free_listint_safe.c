239dc8b9de4ed943f443196d9e40f1d212f7e3e8
demikenc <demikenc@yahoo.co.uk>
2022-10-25 19:57:00 -0700

    more singly

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -25,20 +25,34 @@ size_t print_listint_safe(const listint_t *head)
 						tmp_n = head;
 
 							while (tmp_n)
-							{
-							printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
-							counter++;
-							tmp_n = tmp_n->next;
-							l_n = head;
-							new_n = 0;
-							while (new_n < counter)
-						{
-						if (tmp_n == l_n)
-						{
-					printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
-						return (counter);
-							}
-																					l_n = l_n->next;
+
+									{
+
+												printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
+
+														counter++;
+
+																tmp_n = tmp_n->next;
+
+																		l_n = head;
+
+																				new_n = 0;
+
+																						while (new_n < counter)
+
+																									{
+
+																													if (tmp_n == l_n)
+
+																																	{
+
+																																						printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
+
+																																										return (counter);
+
+																																													}
+
+																																l_n = l_n->next;
 
 																																			new_n++;
 
