d357496ad4f133e1375f300ffcc4c8383285e56c
demikenc <demikenc@yahoo.co.uk>
2022-10-25 20:01:00 -0700

    more singly

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,29 +19,16 @@ size_t free_listint_safe(listint_t **h)
 
 				listint_t *temp;
 
-
-
-					if (!h || !*h)
-
-								return (0);
-
-
-
-						while (*h)
-
-								{
-
-											diff = *h - (*h)->next;
-
-													if (diff > 0)
-
-																{
-
-																				temp = (*h)->next;
-
-																							free(*h);
-
-																										*h = temp;
+				if (!h || !*h)
+				return (0);
+				while (*h)
+				{
+				diff = *h - (*h)->next;
+				if (diff > 0)
+				{
+																			temp = (*h)->next;
+				free(*h);
+																			*h = temp;
 
 																													len++;
 
