8a1682a4381c15668ececf86618970e502794964
demikenc <demikenc@yahoo.co.uk>
2022-10-25 19:16:34 -0700

    more singly

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,29 +1,33 @@
-#include <stdlib.h>
-
-#include <string.h>
-
-#include <stdio.h>
-
 #include "lists.h"
 
 /**
- *  * list_len - Finds the number of elements in
  *
- *   *            a linked list_t list.
+ *  * listint_len - returns the number of elements in a linked lists
  *
- *    * @h: The linked list_t list.
- * Return: The number of elements in h.
+ *   * @h: pointer to the 1st node of the linked list
+ *
+ *    * Return: number of nodes
+ *
  */
 
-size_t list_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 
 {
-	size_t nodenum = 0;
 
-	while (h)
-	{
-	nodenum++;
-	h = h->next;
-		}
-return (nodenum);
+		size_t nodenum = 0;
+
+
+
+			while (h)
+
+					{
+
+								nodenum++;
+
+										h = h->next;
+
+											}
+
+				return (nodenum);
+
 }
