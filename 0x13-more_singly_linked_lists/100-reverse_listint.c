f7344dc47efc109a761311b2a772189e0a215138
demikenc <demikenc@yahoo.co.uk>
2022-10-25 19:31:51 -0700

    more singly

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -21,18 +21,12 @@ listint_t *reverse_listint(listint_t **head)
 
 
 				while (*head)
-
-						{
-
-									next = (*head)->next;
-
-											(*head)->next = prev;
-
-													prev = *head;
-
-															*head = next;
-
-																}
+				{
+				next = (*head)->next;
+				(*head)->next = prev;
+				prev = *head;
+				*head = next;
+					}
 
 
 
